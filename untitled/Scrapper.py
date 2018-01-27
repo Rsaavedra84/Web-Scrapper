e2a947a27d665f9aefff3191da45c04c23d14441
Alieldin Ayman <31348348+alieldinayman@users.noreply.github.com>
2018-01-27 15:54:37 +0200

    CSV Update
    Added CSV support using Pandas

--- untitled/Scrapper.py
+++ untitled/Scrapper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 from urllib import request
+import pandas as pd
 
 class Scrapper:
 
@@ -18,10 +19,13 @@ class Scrapper:
         for likeCount in likes:
             likesT.append(likeCount.text.replace("\n", "").lstrip())  #LEARNT ABOUT RSTRIP AND LSTRIP
 
-        info = list(zip(titles, links, likesT))
+        info = sorted(list(zip(titles, links, likesT)))
 
         for i in range(len(info)):
             print(info[i])
+
+        df = pd.DataFrame(info, columns = ['Titles', 'Links', 'Likes'])
+        df.to_csv("Twitter Data.csv")
 
 
 def main():
